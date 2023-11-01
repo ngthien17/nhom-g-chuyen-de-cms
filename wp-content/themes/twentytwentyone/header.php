89cca84cacf7b846547d691a5bbeb43af5b7a39a
bghao1712 <85864249+bghao1712@users.noreply.github.com>
2023-11-01 08:21:34 +0700

    13

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -27,15 +27,168 @@
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#content">
-			<?php
-			/* translators: Hidden accessibility text. */
-			esc_html_e('Skip to content', 'twentytwentyone');
-			?>
-		</a>
+    <div class="col-md-8">
+        <div class="list_news">
+            <div class="list_new_view">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/6ba9ad9cf9430f7862e80d77fe2b0fe3.jpg">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                              <h2>  <span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+                                <h1>
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/livestream-thiet-ke-do-hoa-phac-hoa-tuong-lai-2023">aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/livestream-thiet-ke-do-hoa-phac-hoa-tuong-lai-2023"></a></p></h1>
+                            </div>
 
-		<?php get_template_part('template-parts/header/site-header'); ?>
-		<div id="content" class="site-content">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list_new_view">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/a86876c5da0267fc844de53bfe93d65b.jpg">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                             <h2>   <span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+                                <h1>
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/livestream-lam-chu-cong-nghe-cung-gen-z">aaaaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>Taaaaaaaaaaaaaaaa<a href="http://fit.tdc.edu.vn/tin-tuc/livestream-lam-chu-cong-nghe-cung-gen-z"></a></p></h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list_new_view">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/368b77395f0f09d88b1676db28f24c31.png">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                               <h2> <span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+                                <h1>
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/livestream-cong-nghe-thong-tin-thoi-dai-gen-z">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>aaaaaaaaaaaa<a href="http://fit.tdc.edu.vn/tin-tuc/livestream-cong-nghe-thong-tin-thoi-dai-gen-z"></a></p></h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list_new_view">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/dad846c1caa03a79621a1e53b61c7e13.jpg">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                                <h2> <span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+                                <h1>
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/livestream-dong-hanh-cung-gen-z-2023">aaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaa<a href="http://fit.tdc.edu.vn/tin-tuc/livestream-dong-hanh-cung-gen-z-2023"></a></p> </h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list_new_view">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/dac69699801120172c5fb8c9e629cd74.jpg">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                              <h2>  <span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+                                <h1>
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/dang-ky-le-tot-nghiep-2023-va-muon-le-phuc">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>aaaaaaaaaaaaaaaaaaaaaa<a href="http://fit.tdc.edu.vn/tin-tuc/dang-ky-le-tot-nghiep-2023-va-muon-le-phuc"></a></p> </h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list_new_view">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/c046f1647158d2e3c5111a5bc0747220.jpg">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                               <h2> <span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+
+                                    <h1>    <a href="http://fit.tdc.edu.vn/tin-tuc/tdc-va-psu-ky-ket-bien-ban-ghi-nho-hop-tac-mou-trong-dao-tao">aaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<a href="http://fit.tdc.edu.vn/tin-tuc/tdc-va-psu-ky-ket-bien-ban-ghi-nho-hop-tac-mou-trong-dao-tao">[...]</a></p></h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list_new_view" >
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="top_news_block_thumb">
+                            <img src="http://fit.tdc.edu.vn/files/large/c8d4dceb675687ad6379e9e2751f9f45.jpg">
+                        </div>
+                    </div>
+                    <div class="col-md-7 top_news_block_desc">
+                        <div class="row">
+                            <div class="col-md-3 col-xs-3 topnewstime">
+                                <h2><span class="topnewsdate">24</span><br>
+                                <span class="topnewsmonth">Tháng 10</span><br></h2>
+                            </div>
+                            <div class="col-md-9 col-xs-9 shortdesc">
+                                <h1>
+                                    <a href="http://fit.tdc.edu.vn/tin-tuc/ky-thi-ky-nang-nghe-tp-ho-chi-minh-nam-2023">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+                                <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<a href="http://fit.tdc.edu.vn/tin-tuc/ky-thi-ky-nang-nghe-tp-ho-chi-minh-nam-2023"></a></p></h1>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
